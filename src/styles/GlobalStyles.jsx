0264f435f4024663657684513cbd92ad644bceea
Taejin Kim <rlaxowls1316@naver.com>
2024-02-24 12:31:27 +0900

    :art: Reset, Global, Font 스타일 전역 적용

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    html{
+        font-size: 62.5%;
+    }
+`;
+
+export default GlobalStyles;
---
+++ src/styles/ResetStyles.jsx
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+const ResetStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        font-family: "Pretendard-Regular";
+        word-break: keep-all;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    input {
+        border: none;
+        padding: none;
+    }
+    input:focus {
+      outline: none;
+    }
+
+    button {
+        border: none;
+        padding: unset;
+        background-color: unset;
+        cursor: pointer;
+    }
+
+`;
+
+export default ResetStyles;
