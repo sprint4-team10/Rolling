37e2074f6714d60f48d8055ad1b5251973d3e73a
Taejin Kim <rlaxowls1316@naver.com>
2024-02-25 23:11:07 +0900

    :art: Prettier 적용

--- src/components/CreateButton/index.jsx
+++ src/components/CreateButton/index.jsx
@@ -1,7 +1,27 @@
-import { CreateBtn } from "./CreateBtn";
+import { CreateBtn } from './CreateBtn';
 
-export function CreateButton({children, widthOption, updownMargin, sideMargin, fontColor , fontSize , bgColor , border}) {
-    return(
-        <CreateBtn type="button" widthOption={widthOption} sideMargin={sideMargin} updownMargin={updownMargin} fontColor={fontColor} fontSize={fontSize} bgColor={bgColor} border={border}>{children}</CreateBtn>
-    );
+export function CreateButton({
+  children,
+  widthOption,
+  updownMargin,
+  sideMargin,
+  fontColor,
+  fontSize,
+  bgColor,
+  border,
+}) {
+  return (
+    <CreateBtn
+      type="button"
+      widthOption={widthOption}
+      sideMargin={sideMargin}
+      updownMargin={updownMargin}
+      fontColor={fontColor}
+      fontSize={fontSize}
+      bgColor={bgColor}
+      border={border}
+    >
+      {children}
+    </CreateBtn>
+  );
 }
