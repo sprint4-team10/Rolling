13302d8fb5a36df78abaf80e50ecb9cedccfc51e
codingaring <sksms2104@naver.com>
2024-02-27 22:08:35 +0900

    ✨ [유경] feat : AddButton 컴포넌트 추가

---
+++ src/components/Buttons/AddButton/AddButton.jsx
@@ -0,0 +1,41 @@
+import COLORS from '../../../utils/colors';
+import styled from 'styled-components';
+import { ReactComponent as Plus } from '../../../assets/icons/plus.svg';
+
+const AddButton = () => {
+  return (
+    <AddButtonContainer type="button">
+      <Plus
+        css={{
+          color: `${COLORS.white}`,
+          width: `2.4rem`,
+          height: `2.4rem`,
+        }}
+      />
+    </AddButtonContainer>
+  );
+};
+
+const AddButtonContainer = styled.button`
+  padding: 1.6rem;
+  background-color: ${COLORS.gray500};
+  border-radius: 10rem;
+
+  &:disabled {
+    background-color: ${COLORS.gray300};
+  }
+
+  &:hover {
+    background-color: ${COLORS.gray600};
+  }
+
+  &:active {
+    background-color: ${COLORS.gray700};
+  }
+
+  &:focus {
+    background-color: ${COLORS.gray800};
+  }
+`;
+
+export default AddButton;
