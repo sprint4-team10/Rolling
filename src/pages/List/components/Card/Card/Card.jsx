5950700bceedf6337e74f2fd787589a70e380d2e
LeeSeungHoon <ysh73900@gmail.com>
2024-02-27 11:26:20 +0900

    ✨[승훈]feat: 리스트페이지 데이터 띄우기 및 디자인#37

--- src/pages/List/components/Card/Card/Card.jsx
+++ src/pages/List/components/Card/Card/Card.jsx
@@ -1,10 +1,16 @@
 import CardContent from '../CardContent/CardContent';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Card = ({ title, backgroundColor, peopleNumber }) => {
+const Card = ({ title, backgroundColor, backgroundImageURL, messageCount, messages, topReactions }) => {
   return (
-    <Wrapper backgroundColor={backgroundColor}>
-      <CardContent title={title} peopleNumber={peopleNumber} />
+    <Wrapper backgroundImageUrl={backgroundImageURL} backgroundColor={backgroundColor}>
+      <CardContent
+        title={title}
+        messageCount={messageCount}
+        messages={messages}
+        backgroundImageURL={backgroundImageURL}
+        topReactions={topReactions}
+      />
     </Wrapper>
   );
 };
@@ -15,12 +21,23 @@ const Wrapper = styled.div`
   padding: 3rem 2.4rem;
   border-radius: 1.6rem;
   border: 0.1rem solid rgba(0, 0, 0, 0.1);
-  background: ${(props) => props.backgroundColor || '#fff'};
   background-size: cover; // 이미지가 컴포넌트 크기에 맞게 조절될 수 있도록
   background-position: center; // 이미지가 가운데 정렬되도록
   box-shadow: 0rem 0.2rem 1.2rem 0rem rgba(0, 0, 0, 0.08);
   min-width: 27.5rem; /* 최소 너비를 원래 크기로 설정 */
   min-height: 26rem; /* 최소 높이를 원래 크기로 설정 */
+
+  ${(props) =>
+    props.backgroundImageUrl &&
+    css`
+      background-image: url(${props.backgroundImageUrl});
+    `}
+
+  ${(props) =>
+    !props.backgroundImageUrl &&
+    css`
+      background-color: ${props.backgroundColor};
+    `}
 `;
 
 export default Card;
