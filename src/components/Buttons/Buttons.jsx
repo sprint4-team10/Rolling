4fa882ae986922b92352f2f86d1b4d72f5e0a025
codingaring <sksms2104@naver.com>
2024-02-28 15:21:24 +0900

    ✨ feat : Buttons컴포넌트에 disabled를 위한 prop 추가

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,19 +1,45 @@
 import { Outlined36, Outlined40, Outlined56, Primary40, Primary56, Secondary40 } from './ButtonsStyled';
 
-const Buttons = ({ children, buttonType, buttonSize }) => {
+const Buttons = ({ children, buttonType, buttonSize, isDisabled }) => {
   switch (buttonType) {
     case 'Primary56':
-      return <Primary56 buttonSize={buttonSize}>{children}</Primary56>;
+      return (
+        <Primary56 buttonSize={buttonSize} isDisabled={isDisabled}>
+          {children}
+        </Primary56>
+      );
     case 'Primary40':
-      return <Primary40 buttonSize={buttonSize}>{children}</Primary40>;
+      return (
+        <Primary40 buttonSize={buttonSize} isDisabled={isDisabled}>
+          {children}
+        </Primary40>
+      );
     case 'Secondary40':
-      return <Secondary40 buttonSize={buttonSize}>{children}</Secondary40>;
+      return (
+        <Secondary40 buttonSize={buttonSize} isDisabled={isDisabled}>
+          {children}
+        </Secondary40>
+      );
     case 'Outlined56':
-      return <Outlined56 buttonSize={buttonSize}>{children}</Outlined56>;
+      return (
+        <Outlined56 buttonSize={buttonSize} isDisabled={isDisabled}>
+          {children}
+        </Outlined56>
+      );
     case 'Outlined40':
-      return <Outlined40 buttonSize={buttonSize}>{children}</Outlined40>;
+      return (
+        <Outlined40 buttonSize={buttonSize} isDisabled={isDisabled}>
+          {children}
+        </Outlined40>
+      );
     case 'Outlined36':
-      return <Outlined36 buttonSize={buttonSize}>{children}</Outlined36>;
+      return (
+        <Outlined36 buttonSize={buttonSize} isDisabled={isDisabled}>
+          {children}
+        </Outlined36>
+      );
+    default:
+      return;
   }
 };
 
