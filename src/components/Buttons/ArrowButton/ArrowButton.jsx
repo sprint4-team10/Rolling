85bd2bea67149330dc8b3f6cb9c6542a8f08d6d3
codingaring <sksms2104@naver.com>
2024-03-04 18:06:48 +0900

    ♻️ [유경] refactor : Add버튼 및 Arrow 버튼 styled 컴포넌트 분리

--- src/components/Buttons/ArrowButton/ArrowButton.jsx
+++ src/components/Buttons/ArrowButton/ArrowButton.jsx
@@ -1,56 +1,27 @@
-import COLORS from '../../../utils/colors';
-import styled from 'styled-components';
 import { ReactComponent as ArrowLeft } from '../../../assets/icons/arrow_left.svg';
 import { ReactComponent as ArrowRight } from '../../../assets/icons/arrow_right.svg';
+import * as S from './ArrowButtonStyled';
 
 const ArrowButton = ({ onClick, direction }) => {
   return direction === 'left' ? (
-    <ArrowButtonContainer type="button" onClick={onClick}>
+    <S.ArrowButtonContainer type="button" onClick={onClick}>
       <ArrowLeft
         css={{
           width: `1.6rem`,
           height: `1.6rem`,
         }}
       />
-    </ArrowButtonContainer>
+    </S.ArrowButtonContainer>
   ) : (
-    <ArrowButtonContainer type="button" onClick={onClick}>
+    <S.ArrowButtonContainer type="button" onClick={onClick}>
       <ArrowRight
         css={{
           width: `1.6rem`,
           height: `1.6rem`,
         }}
       />
-    </ArrowButtonContainer>
+    </S.ArrowButtonContainer>
   );
 };
-
-const ArrowButtonContainer = styled.button`
-  width: 4rem;
-  height: 4rem;
-  padding: 1.2rem;
-  filter: drop-shadow(0px 0.4rem 0.8rem rgba(0, 0, 0, 0.08));
-  backdrop-filter: blur(0.2rem);
-  border-radius: 50%;
-  background-color: ${COLORS.white};
-  border: 1px solid ${COLORS.gray300};
-  opacity: 0.8;
-
-  &:disabled {
-    background-color: ${COLORS.gray100};
-  }
-
-  &:hover {
-    background-color: ${COLORS.gray200};
-  }
-
-  &:active {
-    background-color: ${COLORS.gray300};
-  }
-
-  &:focus {
-    background-color: ${COLORS.gray400};
-  }
-`;
 
 export default ArrowButton;
