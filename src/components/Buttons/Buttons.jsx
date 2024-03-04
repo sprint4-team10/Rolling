4d487bf780489555bc197a92bec872ac15313325
codingaring <sksms2104@naver.com>
2024-03-04 17:13:17 +0900

    ♻️[유경] refactor : Button 컴포넌트 반응형 추가

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,40 +1,76 @@
 import { Outlined36, Outlined40, Outlined56, Primary40, Primary56, Secondary40 } from './ButtonsStyled';
 
-const Buttons = ({ onClick, children, buttonType, buttonSize, isDisabled }) => {
+const Buttons = ({ onClick, children, buttonType, buttonSize, mobileButtonSize, tabletButtonSize, isDisabled }) => {
   switch (buttonType) {
     case 'Primary56':
       return (
-        <Primary56 onClick={onClick} buttonSize={buttonSize} disabled={isDisabled}>
+        <Primary56
+          onClick={onClick}
+          buttonSize={buttonSize}
+          mobileButtonSize={mobileButtonSize}
+          tabletButtonSize={tabletButtonSize}
+          disabled={isDisabled}
+        >
           {children}
         </Primary56>
       );
     case 'Primary40':
       return (
-        <Primary40 onClick={onClick} buttonSize={buttonSize} disabled={isDisabled}>
+        <Primary40
+          onClick={onClick}
+          buttonSize={buttonSize}
+          mobileButtonSize={mobileButtonSize}
+          tabletButtonSize={tabletButtonSize}
+          disabled={isDisabled}
+        >
           {children}
         </Primary40>
       );
     case 'Secondary40':
       return (
-        <Secondary40 onClick={onClick} buttonSize={buttonSize} disabled={isDisabled}>
+        <Secondary40
+          onClick={onClick}
+          buttonSize={buttonSize}
+          mobileButtonSize={mobileButtonSize}
+          tabletButtonSize={tabletButtonSize}
+          disabled={isDisabled}
+        >
           {children}
         </Secondary40>
       );
     case 'Outlined56':
       return (
-        <Outlined56 onClick={onClick} buttonSize={buttonSize} disabled={isDisabled}>
+        <Outlined56
+          onClick={onClick}
+          buttonSize={buttonSize}
+          mobileButtonSize={mobileButtonSize}
+          tabletButtonSize={tabletButtonSize}
+          disabled={isDisabled}
+        >
           {children}
         </Outlined56>
       );
     case 'Outlined40':
       return (
-        <Outlined40 onClick={onClick} buttonSize={buttonSize} isDisabled={isDisabled}>
+        <Outlined40
+          onClick={onClick}
+          buttonSize={buttonSize}
+          mobileButtonSize={mobileButtonSize}
+          tabletButtonSize={tabletButtonSize}
+          isDisabled={isDisabled}
+        >
           {children}
         </Outlined40>
       );
     case 'Outlined36':
       return (
-        <Outlined36 onClick={onClick} buttonSize={buttonSize} isDisabled={isDisabled}>
+        <Outlined36
+          onClick={onClick}
+          buttonSize={buttonSize}
+          mobileButtonSize={mobileButtonSize}
+          tabletButtonSize={tabletButtonSize}
+          isDisabled={isDisabled}
+        >
           {children}
         </Outlined36>
       );
