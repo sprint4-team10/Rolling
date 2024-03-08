d50e40f75c4a3a22147e451a9d3311b95f36bb70
codingaring <sksms2104@naver.com>
2024-03-09 08:55:43 +0900

    💄 [유경] design : favicon 및 페이지별 title 설정

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -8,6 +8,7 @@ import arrowRightIcon from '../../assets/icons/arrow_right.svg';
 import arrowLeftIcon from '../../assets/icons/arrow_left.svg';
 import Buttons from '../../components/Buttons';
 import * as S from './ListStyled';
+import { Helmet } from 'react-helmet-async';
 
 const List = () => {
   const [recipientList, setRecipientList] = useState([]);
@@ -86,93 +87,98 @@ const List = () => {
   }, []);
 
   return (
-    <Layout>
-      <S.Container>
-        <S.Content>
-          <S.Title>인기 롤링 페이퍼 🔥</S.Title>
-          <S.Wrapper>
-            {topSlideX < 0 && (
-              <S.LeftBtn onClick={handleSlideLeftClick} id="top">
-                <img src={arrowLeftIcon} alt="arrowLeftIcon" id="top" />
-              </S.LeftBtn>
-            )}
-            {topSlideX > recipientListWidth.current && (
-              <S.RightBtn onClick={handleSlideRightClick} id="top">
-                <img src={arrowRightIcon} alt="arrowRightIcon" id="top" />
-              </S.RightBtn>
-            )}
-            <S.CardListContainer
-              ref={topContainerRef}
-              onMouseDown={(e) => handleMouseDownEvent(e, topContainerRef)}
-              onMouseLeave={() => setDragging(false)}
-              onMouseUp={() => setDragging(false)}
-              onMouseMove={(e) => handleMouseMoveEvent(e, topContainerRef)}
-            >
-              <CardList slideX={topSlideX}>
-                {popularRecipientList.map((card) => (
-                  <Card
-                    key={card.id}
-                    id={card.id}
-                    title={card.name}
-                    backgroundColor={card.backgroundColor}
-                    backgroundImageURL={card.backgroundImageURL}
-                    messageCount={card.messageCount}
-                    messages={card.recentMessages}
-                    topReactions={card.topReactions}
-                  />
-                ))}
-              </CardList>
-            </S.CardListContainer>
-          </S.Wrapper>
-        </S.Content>
+    <>
+      <Helmet>
+        <title>Rolling 롤링페이퍼 목록</title>
+      </Helmet>
+      <Layout>
+        <S.Container>
+          <S.Content>
+            <S.Title>인기 롤링 페이퍼 🔥</S.Title>
+            <S.Wrapper>
+              {topSlideX < 0 && (
+                <S.LeftBtn onClick={handleSlideLeftClick} id="top">
+                  <img src={arrowLeftIcon} alt="arrowLeftIcon" id="top" />
+                </S.LeftBtn>
+              )}
+              {topSlideX > recipientListWidth.current && (
+                <S.RightBtn onClick={handleSlideRightClick} id="top">
+                  <img src={arrowRightIcon} alt="arrowRightIcon" id="top" />
+                </S.RightBtn>
+              )}
+              <S.CardListContainer
+                ref={topContainerRef}
+                onMouseDown={(e) => handleMouseDownEvent(e, topContainerRef)}
+                onMouseLeave={() => setDragging(false)}
+                onMouseUp={() => setDragging(false)}
+                onMouseMove={(e) => handleMouseMoveEvent(e, topContainerRef)}
+              >
+                <CardList slideX={topSlideX}>
+                  {popularRecipientList.map((card) => (
+                    <Card
+                      key={card.id}
+                      id={card.id}
+                      title={card.name}
+                      backgroundColor={card.backgroundColor}
+                      backgroundImageURL={card.backgroundImageURL}
+                      messageCount={card.messageCount}
+                      messages={card.recentMessages}
+                      topReactions={card.topReactions}
+                    />
+                  ))}
+                </CardList>
+              </S.CardListContainer>
+            </S.Wrapper>
+          </S.Content>
 
-        <S.Content>
-          <S.Title>최근에 만든 롤링 페이퍼 ⭐️️</S.Title>
-          <S.Wrapper>
-            {bottomSlideX < 0 && (
-              <S.LeftBtn onClick={handleSlideLeftClick} id="bottom">
-                <img src={arrowLeftIcon} alt="arrowLeftIcon" id="bottom" />
-              </S.LeftBtn>
-            )}
-            {bottomSlideX > recipientListWidth.current && (
-              <S.RightBtn onClick={handleSlideRightClick} id="bottom">
-                <img src={arrowRightIcon} alt="arrowRightIcon" id="bottom" />
-              </S.RightBtn>
-            )}
-            <S.CardListContainer
-              ref={bottomContainerRef}
-              onMouseDown={(e) => handleMouseDownEvent(e, bottomContainerRef)}
-              onMouseLeave={() => setDragging(false)}
-              onMouseUp={() => setDragging(false)}
-              onMouseMove={(e) => handleMouseMoveEvent(e, bottomContainerRef)}
-            >
-              <CardList slideX={bottomSlideX}>
-                {recipientList.map((card) => (
-                  <Card
-                    key={card.id}
-                    id={card.id}
-                    title={card.name}
-                    backgroundColor={card.backgroundColor}
-                    backgroundImageURL={card.backgroundImageURL}
-                    messageCount={card.messageCount}
-                    messages={card.recentMessages}
-                    topReactions={card.topReactions}
-                  />
-                ))}
-              </CardList>
-            </S.CardListContainer>
-          </S.Wrapper>
-        </S.Content>
+          <S.Content>
+            <S.Title>최근에 만든 롤링 페이퍼 ⭐️️</S.Title>
+            <S.Wrapper>
+              {bottomSlideX < 0 && (
+                <S.LeftBtn onClick={handleSlideLeftClick} id="bottom">
+                  <img src={arrowLeftIcon} alt="arrowLeftIcon" id="bottom" />
+                </S.LeftBtn>
+              )}
+              {bottomSlideX > recipientListWidth.current && (
+                <S.RightBtn onClick={handleSlideRightClick} id="bottom">
+                  <img src={arrowRightIcon} alt="arrowRightIcon" id="bottom" />
+                </S.RightBtn>
+              )}
+              <S.CardListContainer
+                ref={bottomContainerRef}
+                onMouseDown={(e) => handleMouseDownEvent(e, bottomContainerRef)}
+                onMouseLeave={() => setDragging(false)}
+                onMouseUp={() => setDragging(false)}
+                onMouseMove={(e) => handleMouseMoveEvent(e, bottomContainerRef)}
+              >
+                <CardList slideX={bottomSlideX}>
+                  {recipientList.map((card) => (
+                    <Card
+                      key={card.id}
+                      id={card.id}
+                      title={card.name}
+                      backgroundColor={card.backgroundColor}
+                      backgroundImageURL={card.backgroundImageURL}
+                      messageCount={card.messageCount}
+                      messages={card.recentMessages}
+                      topReactions={card.topReactions}
+                    />
+                  ))}
+                </CardList>
+              </S.CardListContainer>
+            </S.Wrapper>
+          </S.Content>
 
-        <S.ButtonContainer>
-          <Link to="/post">
-            <Buttons buttonType="Primary56" buttonSize="small" tabletButtonSize="full">
-              나도 만들어보기
-            </Buttons>
-          </Link>
-        </S.ButtonContainer>
-      </S.Container>
-    </Layout>
+          <S.ButtonContainer>
+            <Link to="/post">
+              <Buttons buttonType="Primary56" buttonSize="small" tabletButtonSize="full">
+                나도 만들어보기
+              </Buttons>
+            </Link>
+          </S.ButtonContainer>
+        </S.Container>
+      </Layout>
+    </>
   );
 };
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,46 +4,52 @@ import Layout from '../../layout/Layout';
 import { Link } from 'react-router-dom';
 import Buttons from '../../components/Buttons';
 import * as S from './MainStyled';
+import { Helmet } from 'react-helmet-async';
 
 const Main = () => {
   return (
-    <S.Container>
-      <Layout>
-        <S.Section>
-          <S.Contents>
-            <S.Point>Point. 01</S.Point>
-            <S.Title>
-              누구나 손쉽게, 온라인
-              <S.Br /> 롤링 페이퍼를 만들 수 있어요
-            </S.Title>
-            <S.Description>로그인 없이 자유롭게 만들어요.</S.Description>
-          </S.Contents>
-          <S.Cover>
-            <img src={landing01} alt="landingCover01" />
-          </S.Cover>
-        </S.Section>
-        <S.Section>
-          <S.Contents>
-            <S.Point>Point. 02</S.Point>
-            <S.Title>
-              서로에게 이모지로 감정을
-              <S.Br /> 표현해보세요.
-            </S.Title>
-            <S.Description>롤링 페이퍼에 이모지를 추가할 수 있어요.</S.Description>
-          </S.Contents>
-          <S.Cover>
-            <img src={landing02} alt="landingCover01" />
-          </S.Cover>
-        </S.Section>
-        <Link to="/list">
-          <div style={{ textAlign: 'center' }}>
-            <Buttons buttonType="Primary56" buttonSize="small" tabletButtonSize="full">
-              구경해보기
-            </Buttons>
-          </div>
-        </Link>
-      </Layout>
-    </S.Container>
+    <>
+      <Helmet>
+        <title>Rolling</title>
+      </Helmet>
+      <S.Container>
+        <Layout>
+          <S.Section>
+            <S.Contents>
+              <S.Point>Point. 01</S.Point>
+              <S.Title>
+                누구나 손쉽게, 온라인
+                <S.Br /> 롤링 페이퍼를 만들 수 있어요
+              </S.Title>
+              <S.Description>로그인 없이 자유롭게 만들어요.</S.Description>
+            </S.Contents>
+            <S.Cover>
+              <img src={landing01} alt="landingCover01" />
+            </S.Cover>
+          </S.Section>
+          <S.Section>
+            <S.Contents>
+              <S.Point>Point. 02</S.Point>
+              <S.Title>
+                서로에게 이모지로 감정을
+                <S.Br /> 표현해보세요.
+              </S.Title>
+              <S.Description>롤링 페이퍼에 이모지를 추가할 수 있어요.</S.Description>
+            </S.Contents>
+            <S.Cover>
+              <img src={landing02} alt="landingCover01" />
+            </S.Cover>
+          </S.Section>
+          <Link to="/list">
+            <div style={{ textAlign: 'center' }}>
+              <Buttons buttonType="Primary56" buttonSize="small" tabletButtonSize="full">
+                구경해보기
+              </Buttons>
+            </div>
+          </Link>
+        </Layout>
+      </S.Container>
+    </>
   );
 };
 
