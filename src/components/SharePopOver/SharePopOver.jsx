0fb11045d50e2b66fee8bf869887356fadc20e8a
Taejin Kim <rlaxowls1316@naver.com>
2024-03-08 21:19:33 +0900

    :sparkles: [태진] feat: 카카오톡 공유기능 구현

--- src/components/SharePopOver/SharePopOver.jsx
+++ src/components/SharePopOver/SharePopOver.jsx
@@ -1,8 +1,10 @@
 import PopOver from '../PopOver';
-import { forwardRef, useState } from 'react';
+import { forwardRef, useState, useEffect } from 'react';
 import { ModalPortal } from '../Portal';
 import Toast from '../Toast';
 import * as S from './SharePopOverStyled';
+
+const { Kakao } = window;
 
 const SharePopOver = forwardRef(({ isOpen, style, handleClose }, ref) => {
   const [showToast, setShowToast] = useState(false);
@@ -18,6 +20,35 @@ const SharePopOver = forwardRef(({ isOpen, style, handleClose }, ref) => {
     handleClose();
   };
 
+  const kakaoShare = () => {
+    Kakao.Share.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: 'Rolling',
+        description: '추억의 롤링페이퍼가 웹 속으로!',
+        imageUrl: 'https://github.com/sprint4-team10/Rolling/assets/153581513/27f04c57-df47-4537-81cd-49942e794526',
+        link: {
+          mobileWebUrl: 'https://main--stirring-youtiao-0163ec.netlify.app/',
+        },
+      },
+      buttons: [
+        {
+          title: '보러가기',
+          link: {
+            mobileWebUrl: 'https://main--stirring-youtiao-0163ec.netlify.app/',
+          },
+        },
+      ],
+    });
+  };
+
+  useEffect(() => {
+    Kakao.cleanup();
+    console.log('key:', process.env.REACT_APP_KAKAO_KEY);
+    Kakao.init(process.env.REACT_APP_KAKAO_KEY);
+    console.log('check!!:', Kakao.isInitialized());
+  }, []);
+
   return (
     <>
       {showToast && (
@@ -27,7 +58,7 @@ const SharePopOver = forwardRef(({ isOpen, style, handleClose }, ref) => {
       )}
       <PopOver popOverRef={ref} isOpen={isOpen} style={style}>
         <S.Container>
-          <S.Btn>카카오톡 공유</S.Btn>
+          <S.Btn onClick={kakaoShare}>카카오톡 공유</S.Btn>
           <S.Btn onClick={handleUrlShare}>URL 공유</S.Btn>
         </S.Container>
       </PopOver>
