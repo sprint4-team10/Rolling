5b07cad37d11b1c7aef380333fd2233475058fff
codingaring <sksms2104@naver.com>
2024-03-09 09:44:45 +0900

    🐛 fix : checkMark 로딩시 세로로 찌그러지는 문제 개선

--- src/pages/Post/components/CheckMark/CheckMarkStyled.js
+++ src/pages/Post/components/CheckMark/CheckMarkStyled.js
@@ -3,6 +3,7 @@ import COLORS from '../../../../styles/colors';
 
 export const Check = styled.div`
   min-width: 4.4rem;
+  min-height: 4.4rem;
   padding: 1rem;
   background-color: ${COLORS.gray500};
   border-radius: 50%;
